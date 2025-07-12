25ba2438b6c727fbac8b393127bd8c5dd0fde71d
LekhivOleh <lekhivoleg@gmail.com>
2025-07-12 23:02:18 +0300

    feat: migrations with updating user constraints

--- Migrations/20250712193456_AddUniqueConstraintUserEmail.Designer.cs
+++ Migrations/20250712193456_AddUniqueConstraintUserEmail.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using learner;
@@ -11,9 +12,11 @@ using learner;
 namespace learner.Migrations
 {
     [DbContext(typeof(LearnerDbContext))]
-    partial class LearnerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250712193456_AddUniqueConstraintUserEmail")]
+    partial class AddUniqueConstraintUserEmail
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
